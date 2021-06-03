463e6933f577a29838a834c3133d3ed3a0c3cf03
holpipe <holmanf.ruizr@konradlorenz.edu.co>
2021-06-03 13:08:12 -0500

    Se agregaron otros 2 slide en la pagina explorar y se enlazo el home con la pagina explorar

--- js/jsExplorador.js
+++ js/jsExplorador.js
@@ -1,3 +1,5 @@
+
+//---------slide 1--------
 const fila = document.querySelector('.contenedorSlide');
 const videos = document.querySelectorAll('.videos');
 
@@ -57,5 +59,67 @@ videos.forEach((video) => {
 
 fila.addEventListener('mouseleave', () => {
 	videos.forEach(video => video.classList.remove('hover'));
+});
+
+//---------slide 2--------
+const fila2 = document.querySelector('.contenedorSlide2');
+const videos2 = document.querySelectorAll('.videos2');
+
+const flechaIzquierda2 = document.getElementById('flecha-izquierda2');
+const flechaDerecha2 = document.getElementById('flecha-derecha2');
+
+//---flecha derecha2---
+flechaDerecha2.addEventListener('click', () => {
+	fila2.scrollLeft += fila2.offsetWidth;
+
+	const indicadorActivo2 = document.querySelector('.indicadores2 .activo');
+	if(indicadorActivo2.nextSibling){
+		indicadorActivo2.nextSibling.classList.add('activo');
+		indicadorActivo2.classList.remove('activo');
+	}
+});
+
+//---- flecha izquierda2----
+flechaIzquierda2.addEventListener('click', () => {
+	fila2.scrollLeft -= fila2.offsetWidth;
+
+	const indicadorActivo2 = document.querySelector('.indicadores2 .activo');
+	if(indicadorActivo2.previousSibling){
+		indicadorActivo2.previousSibling.classList.add('activo');
+		indicadorActivo2.classList.remove('activo');
+	}
+});
+
+//  ----- Paginacion2 ---
+const numeroPaginas2 = Math.ceil(videos2.length / 5);
+for(let i = 0; i < numeroPaginas2; i++){
+	const indicador2 = document.createElement('button');
+
+	if(i === 0){
+		indicador2.classList.add('activo');
+	}
+
+	document.querySelector('.indicadores2').appendChild(indicador2);
+	indicador2.addEventListener('click', (e) => {
+		fila2.scrollLeft = i * fila2.offsetWidth;
+
+		document.querySelector('.indicadores2 .activo').classList.remove('activo');
+		e.target.classList.add('activo');
+	});
+}
+
+//  ----- Hover 2----
+videos2.forEach((video2) => {
+	videos2.addEventListener('mouseenter2', (e) => {
+		const elemento2 = e.currentTarget;
+		setTimeout(() => {
+			videos2.forEach(video => video2.classList.remove('hover'));
+			elemento2.classList.add('hover');
+		}, 300);
+	});
+});
+
+fila2.addEventListener('mouseleave2', () => {
+	videos2.forEach(video2 => video2.classList.remove('hover'));
 });
 
