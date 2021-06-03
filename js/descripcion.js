5b65878756d497c7abcb5b341b915ea223a8d692
ianfa2201 <ianf.blancol@konradlorenz.edu.co>
2021-06-02 23:28:13 -0500

    Se agrego el archivo video2 y se agrego una funcion js para ocultar y mostrar la descripcion del video y unas imagenes para los botones

---
+++ js/descripcion.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("#drop-up").click(function() {
+        $(".descripcion").hide();
+    });
+});
+
+$(function() {
+    $("#drop-down").click(function() {
+        $(".descripcion").show();
+    });
+});
